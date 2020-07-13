c361ac86b391b0be07bb172bf0088677b096555b
raymond wang <waymondrang@gmail.com>
2020-07-13 12:29:25 -0700

    pre github

--- commands/apex.js
+++ commands/apex.js
@@ -6,7 +6,7 @@ async function apex(discord, msg, mongo, commands, content, config) {
     embed.setFooter('Echelon v1.0')
     if (content[1]) {
         if (commands.apex['more-info']['platforms'].indexOf(content[1].toLowerCase()) !== -1) {
-            var response = await fetch(`https://api.mozambiquehe.re/bridge?version=4&platform=${content[1].toUpperCase()}&player=${content[2]}&auth=w8wMWKuEQgcX0DdN5L5s`).then(res => res.json())
+            var response = await fetch(`https://api.mozambiquehe.re/bridge?version=4&platform=${content[1].toUpperCase()}&player=${content[2]}&auth=${config['apextoken']}`).then(res => res.json())
             if (typeof response.global == 'undefined') {
                 embed.setTitle('Player not found!')
                 embed.setDescription(`Please verify the \`platform\` and \`username\` before retrying!`)
@@ -14,11 +14,12 @@ async function apex(discord, msg, mongo, commands, content, config) {
                 msg.channel.send(embed)
             } else {
                 //console.log(response)
-                embed.setTitle(`\`${response.global.name}'s stats\``)
+                embed.setTitle(`\`${response.global.name}\` Apex Legends Stats`)
+                embed.setThumbnail(response.legends.selected['ImgAssets'].icon)
                 embed.addField('`Level`', response.global.level, true)
-                embed.addField('`Rank`', `${response.global.rank.rankName.toLowerCase()} ${response.global.rank.rankDiv}`, true)
+                embed.addField('`Rank`', `${response.global.rank.rankName} ${response.global.rank.rankDiv}`, true)
                 for (item in response.total) {
-                    embed.addField(`\`${response.total[item].name.toLowerCase()}\``, response.total[item].value, true)
+                    embed.addField(`\`${response.total[item].name}\``, response.total[item].value, true)
                 }
                 embed.setColor(`0x${config['colors'][Math.floor(Math.random() * config['colors'].length)]}`)
                 msg.channel.send(embed)
